ca72585b81c3f42389bddb7b204d36a02352f8d5
lestetedelioncourt <37556899+lestetedelioncourt@users.noreply.github.com>
2018-04-26 19:12:12 +0100

    sgl connection

--- includes1-db.php
+++ includes1-db.php
@@ -7,7 +7,7 @@
 	$conn = mysqli_connect($server, $user,$password,$db);
 	
 	if(!$conn){
-		die("Connection failed!".mysql1_connect_error());
+		die("Connection failed!".mysqli_connect_error());
 	}
 
 ?>
